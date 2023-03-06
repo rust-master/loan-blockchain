fe213663c59eaef67836dd38e740b8a8d4973e66
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2023-03-06 10:23:31 +0500

    Approve loan

--- x/loan/keeper/msg_server_approve_loan.go
+++ x/loan/keeper/msg_server_approve_loan.go
@@ -3,15 +3,33 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"loan/x/loan/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) ApproveLoan(goCtx context.Context, msg *types.MsgApproveLoan) (*types.MsgApproveLoanResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
+	loan, found := k.GetLoan(ctx, msg.Id)
+	if !found {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrKeyNotFound, "key %d doesn't exist", msg.Id)
+	}
+	if loan.State != "requested" {
+		return nil, sdkerrors.Wrapf(types.ErrWrongLoanState, "%v", loan.State)
+	}
+	lender, _ := sdk.AccAddressFromBech32(msg.Creator)
+	borrower, _ := sdk.AccAddressFromBech32(loan.Borrower)
+	amount, err := sdk.ParseCoinsNormalized(loan.Amount)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrWrongLoanState, "Cannot parse coins in loan amount")
+	}
+	err = k.bankKeeper.SendCoins(ctx, lender, borrower, amount)
+	if err != nil {
+		return nil, err
+	}
+	loan.Lender = msg.Creator
+	loan.State = "approved"
+	k.SetLoan(ctx, loan)
 	return &types.MsgApproveLoanResponse{}, nil
 }
